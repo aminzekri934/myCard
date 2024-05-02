8b32604318bf9b898b46ce237f42d12d40306650
aminzekri934 <aminzekri934@gmail.com>
2024-05-02 06:50:15 +0100

    Initial Commit

---
+++ Amine-Card/Amine_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amine_CardApp.swift
+//  Amine-Card
+//
+//  Created by Amine Zekri on 2/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Amine_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Amine-Card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Amine-Card
+//
+//  Created by Amine Zekri on 2/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
